b040b7118512113a27f3ea90c4143782a3fef5cc
Raúl-Labrador <raullabrador10@gmail.com>
2025-12-09 16:58:14 +0100

    Subida de archivos. recopilados del trabajo de DNS

---
+++ bootstrap.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -xeu
+apt-get update -y
+apt-get upgrade -y
+
+apt-get install -y bind9 bind9utils bind9-doc
+
+sed -i 's/^OPTIONS=.*/OPTIONS="-u bind -4"/' /etc/default/named
+
+mkdir -p /var/lib/bind
+
+cp /vagrant/config/named.conf.options /etc/bind/
+cp /vagrant/config/named.conf.local /etc/bind/
+cp /vagrant/zones/raul.test.dns /var/lib/bind/
+cp /vagrant/zones/raul.test.rev /var/lib/bind/
+
+named-checkconf /etc/bind/named.conf.options
+named-checkzone raul.test /var/lib/bind/raul.test.dns
+named-checkzone 56.168.192.in-addr.arpa /var/lib/bind/raul.test.rev
+
+systemctl restart named
+systemctl restart bind9
+systemctl enable named
+systemctl enable bind9
+
+systemctl status named --no-pager
+
